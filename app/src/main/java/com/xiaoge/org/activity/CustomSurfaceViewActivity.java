9976c9109b82bff67e1892b88537cf0e701d2476
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-07-17 11:37:37 +0800

    log

--- app/src/main/java/com/xiaoge/org/activity/CustomSurfaceViewActivity.java
+++ app/src/main/java/com/xiaoge/org/activity/CustomSurfaceViewActivity.java
@@ -3,14 +3,26 @@ package com.xiaoge.org.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.xiaoge.org.R;
 
 public class CustomSurfaceViewActivity extends AppCompatActivity {
+    public static final String TAG = CustomSurfaceViewActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_surface_view);
+        Log.i(TAG, "pid: " + android.os.Process.myPid());
+
+        com.org.zw.utils.LogUtil.i(TAG, "hello log");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                com.org.zw.utils.LogUtil.i(TAG, "hello Thread");
+            }
+        }).start();
     }
 }
