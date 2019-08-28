10dc1bd7e1aadd12e38927d168e8dfc5686d7c81
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-08-28 14:07:28 +0800

    note

--- app/src/main/java/com/xiaoge/org/activity/demo/WindowManagerActivity.java
+++ app/src/main/java/com/xiaoge/org/activity/demo/WindowManagerActivity.java
@@ -6,11 +6,13 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,9 +22,9 @@ import android.widget.LinearLayout;
 
 import com.xiaoge.org.R;
 
-import static android.view.WindowManager.LayoutParams.TYPE_TOAST;
-
 public class WindowManagerActivity extends AppCompatActivity {
+    public static final String TAG = WindowManagerActivity.class.getSimpleName();
+
     @BindView(R.id.ll_container)
     LinearLayout ll_container;
 
@@ -41,23 +43,33 @@ public class WindowManagerActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Windowmanager 添加一个view
+     * 可以模拟Toast
+     */
     @OnClick(R.id.btn_wm)
     void btn_wm() {
+        Log.i(TAG, "btn_wm");
+
         WindowManager windowManager = getWindowManager();
-        Button view = new Button(WindowManagerActivity.this);
-        view.setText("hello float");
+        View view = View.inflate(WindowManagerActivity.this, R.layout.wm_layout, null);
+
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT,
-                0, 0, PixelFormat.TRANSPARENT
+                0, 0, PixelFormat.OPAQUE
         );
         params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
         params.gravity = Gravity.CENTER;
-        params.x = 300;
-        params.y = 200;
-        params.type = TYPE_TOAST;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            params.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+
         windowManager.addView(view, params);
     }
 }
