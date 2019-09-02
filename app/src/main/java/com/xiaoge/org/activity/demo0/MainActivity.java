fc3d888e0594e2ef9c1aa6bc1fe95ada764040be
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-09-02 17:37:35 +0800

    note

--- app/src/main/java/com/xiaoge/org/activity/demo0/MainActivity.java
+++ app/src/main/java/com/xiaoge/org/activity/demo0/MainActivity.java
@@ -21,7 +21,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
