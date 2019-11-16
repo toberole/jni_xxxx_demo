7c25184890e4e16df2eafea67298563762982b87
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-11-16 12:34:23 +0800

    note

--- app/src/main/java/com/xiaoge/org/test/Test22.java
+++ app/src/main/java/com/xiaoge/org/test/Test22.java
@@ -34,28 +34,32 @@ public class Test22 {
 
     /**
      * 奇数位于偶数前面 保持元素的前后相对位置不变
+     * <p>
      * 用空间换时间，如果遇到奇数就用一个数组存起来，
      * 遇到偶数再用另一个数组存起来就需要2个额外的数组，
-     * 再最后合并到一个数组里，这是一个思路（或者2个队列也是同样的思路），
-     * 现在这里优化一下，只申请一个额外的数组，将原来的数组从左往右扫，
+     * 再最后合并到一个数组里。
+     * 优化一下，只申请一个额外的数组，将原来的数组从左往右扫，
      * 遇到奇数就存到新数组的左边，同时将原来的数组从右往左扫，
-     * 遇到偶数就存到新数组的右边，这样就可以保证左边是奇数，右边是偶数，且奇数之间、偶数之间相对位置不变，再合并到原数组。
+     * 遇到偶数就存到新数组的右边，这样就可以保证左边是奇数，
+     * 右边是偶数，且奇数之间、偶数之间相对位置不变，再合并到原数组。
      */
     public static void move4(int[] array) {
         int len = array.length;
-        int[] arr = new int[len];
+        int[] temp = new int[len];
         int start = 0;
         int end = len - 1;
         for (int i = 0; i < len; ++i) {
             if ((array[i] & 1) == 1) {
-                arr[start++] = array[i];
+                temp[start++] = array[i];
             }
+
             if ((array[len - 1 - i] & 1) == 0) {
-                arr[end--] = array[len - 1 - i];
+                temp[end--] = array[len - 1 - i];
             }
         }
+
         for (int i = 0; i < len; ++i) {
-            array[i] = arr[i];
+            array[i] = temp[i];
         }
     }
 
@@ -141,7 +145,41 @@ public class Test22 {
 }
 
 
+class ReOrder {
 
+    public void reOrderArray(int[] array) {
+        if (array.length == 0 || array == null)
+            return;
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            if (array[i] % 2 == 1) {            //如果是奇数
+                int j = i - 1;
+                for (; j >= 0 && array[j] % 2 == 0; j--) {//奇数前面的所有偶数都得后移
+                    array[j + 1] = array[j];
+                }
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    public void reOrder(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            if (arr[i] % 2 == 1) {//如果是奇数
+
+                for (int j = i; j > 0; j--) {
+                    if (arr[j - 1] % 2 == 0) {//奇数前面的所有偶数交换
+                        int t = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = t;
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 
